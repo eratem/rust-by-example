fdb6436a16e03c1945c3a97dbc402540a2b3d5a3
eratem <unasinni@gmail.com>
2023-06-18 20:31:43 +0200

    add comments to hello world

--- hello_world.rs
+++ hello_world.rs
@@ -1,3 +1,8 @@
 fn main() {
+    // Simple line comment
+    /* Block comment that goes
+    until here*/
+    /// Doc comment for HTML documentation for following item
+    //! Did not yet understand this one, enclosing item could mean the block bellow
     println!("Hello World\nI'm a Rustacean!");
 }
