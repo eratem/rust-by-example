3cd899dbf4441e9609a43374873e794337f3af1a
René Mettler <rmettler@anybotics.com>
2023-09-13 12:35:10 +0200

    add guards

---
+++ src/bin/42_guards.rs
@@ -0,0 +1,26 @@
+#[allow(dead_code)]
+enum Temperature {
+    Celsius(i32),
+    Fahrenheit(i32),
+}
+
+fn main() {
+    let temperature = Temperature::Celsius(35);
+    // TODO ^ try different values for `temperature`
+
+    match temperature {
+        Temperature::Celsius(t) if t > 25 => println!("{}°C is above 25°C", t),
+        Temperature::Celsius(t) => println!("{}°C is below 25°C", t),
+        Temperature::Fahrenheit(t) if t > 86 => println!("{}F is above 86 Fahrenheit", t),
+        Temperature::Fahrenheit(t) => println!("{}F is below 86 Fahrenheit", t),
+    }
+
+    let number: u8 = 4;
+
+    match number {
+        i if i == 0 => println!("Zero"),
+        i if i > 0 => println!("Greater than zero"),
+        _ => unreachable!("Should never happen."),
+        // TODO ^ uncomment to fix compilation
+    }
+}
