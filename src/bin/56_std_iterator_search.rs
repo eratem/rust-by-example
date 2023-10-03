247949bc0f9386518361d2464a914fc7f968ffbd
René Mettler <rmettler@anybotics.com>
2023-10-03 14:56:10 +0200

    extend iterator search with position()

--- src/bin/56_std_iterator_search.rs
+++ src/bin/56_std_iterator_search.rs
@@ -22,4 +22,13 @@ fn main() {
         "Find 2 in array2: {:?}",
         array2.into_iter().find(|&x| x == 2)
     );
+
+    let vec = vec![1, 9, 3, 3, 13, 2];
+    // `iter()` for vecs yields a reference and `position()` does not take a
+    // reference, we have to destructure `&i32` to `i32`
+    let index_of_first_even_number = vec.iter().position(|&x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+
+    let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
+    assert_eq!(index_of_first_negative_number, None);
 }
